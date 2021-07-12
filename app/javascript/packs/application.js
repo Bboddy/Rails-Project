6dd271d8d7bc7a392578ef24125565257301de87
bryce_boddy <bryce.boddy@gmail.com>
2021-07-12 02:36:54 -0700

    nav bar upgrade and moved alerts and notifications to _messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 import 'bootstrap'
+import 'bootstrap/dist/js/bootstrap'
+import "bootstrap/dist/css/bootstrap";
 import "stylesheets/application"
 
 Rails.start()
