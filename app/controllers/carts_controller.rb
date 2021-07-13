9b9c2c65489e5ed891712e17d0d954705621ba15
bryce_boddy <bryce.boddy@gmail.com>
2021-07-13 04:39:34 -0700

    creation of of views for carts, categories, orders, and products. along with some styling changes for the login page.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,54 @@
 class CartsController < ApplicationController
+    def index
+        
+    end
+
+    def create # current_user ? current_user.name : 'Rails Store'
+        cart = Cart.find_or_create_by(user: current_user)
+        product = Product.find(params[:product_id])
+        item = Item.find_or_create_by(product: product, cart: cart)
+        item.quanity ? item.update(quanity: (item.quanity + params[:quantiy].to_i)) : item.update(quanity: params[:quantiy])
+        update_total_price()
+        redirect_to cart_path(cart.id)
+    end
+
+    def show
+        @cart = Cart.find_or_create_by(user: current_user)
+        @items = Item.where(cart: @cart)
+        render 'carts/show'
+    end
+
+    def update
+        cart = Cart.find_or_create_by(user: current_user)
+        product = Product.find(params[:product_id])
+        item = Item.find_or_create_by(product: product, cart: cart)
+        
+        if item_params[:quanity].to_i < 1
+            item.destroy
+            update_total_price()
+        else
+            item.update(item_params)
+            update_total_price()
+        end
+
+        redirect_to cart_path(cart.id)
+    end
+
+    private
+
+    def item_params
+        params.require(:item).permit(:quanity)
+    end
+
+    def update_total_price
+        cart = Cart.find_or_create_by(user: current_user)
+        items = Item.where(cart: cart)
+        total = 0
+        
+        items.each do |i|
+            total += (i.product.price.to_i * i.quanity.to_i)
+        end
+
+        cart.update(total_price: total.to_i)
+    end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,7 @@
 class OrdersController < ApplicationController
+    def new
+        cart = Cart.find_or_create_by(user: current_user)
+        @items = Item.where(cart: cart)
+        render 'orders/new'
+    end
 end
