463a163616b738631f6768ea05d5dcd8445a88f9
bryce_boddy <bryce.boddy@gmail.com>
2021-07-14 22:49:59 -0700

    cart and order views and controllers setup

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -2,6 +2,20 @@ class OrderController < ApplicationController
     def new
         cart = Cart.find_or_create_by(user: current_user)
         @items = Item.where(cart: cart)
+        @order_total = 0
+        @items. each do |i|
+            @order_total = @order_total + (i.product.price * i.quanity)
+        end
         render 'orders/new'
+    end
+
+    def show
+        cart = Cart.find_or_create_by(user: current_user)
+        @items = Item.where(cart: cart)
+        @order_total = 0
+        @items. each do |i|
+            @order_total = @order_total + (i.product.price * i.quanity)
+        end
+        render 'orders/show'
     end
 end
