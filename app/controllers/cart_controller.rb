349d0a2a390fa6d625523c907beb599b24c5527a
bryce_boddy <bryce.boddy@gmail.com>
2021-07-17 01:25:15 -0700

    _header fix for non-signed in users

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -3,7 +3,7 @@ class CartController < ApplicationController
         
     end
 
-    def create # current_user ? current_user.name : 'Rails Store'
+    def create
         cart = Cart.find_or_create_by(user: current_user)
         product = Product.find(params[:product_id])
         item = Item.find_or_create_by(product: product, cart: cart)
